153a0d2ac36329796303f49a9188e6435b94dbbb
CodeWithKashish29 <kashishpramodkumar.patel@student.sl.on.ca>
2025-12-08 17:21:53 -0500

    fidex coin and flag sprites

--- Assets/Scripts/CoinScript.cs
+++ Assets/Scripts/CoinScript.cs
@@ -4,8 +4,11 @@ public class CoinScript : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Line 7");
         if (collision.CompareTag("Player"))
         {
+
+            Debug.Log("Line 11");
             PlayerController player = collision.GetComponent<PlayerController>();
 
             if (player != null)
@@ -13,7 +16,7 @@ public class CoinScript : MonoBehaviour
                 player.coins += 1;
             }
 
-            SoundManager.Instance.PlaySFX("COIN", 0.4f);   // 🔊 NEW – 40% volume
+           // SoundManager.Instance.PlaySFX("COIN", 0.4f);   // 🔊 NEW – 40% volume
             Destroy(gameObject);
         }
     }
